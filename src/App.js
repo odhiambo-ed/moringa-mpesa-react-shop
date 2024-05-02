07312d37ac275b5dbd7dc44de30e447c70212e34
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-02 23:45:56 +0300

    add header logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import StoreIcon from '@mui/icons-material/Store';
 
 function App() {
   return (
     <div className="App">
       <div className="header__logo">
-
+        <StoreIcon />
+        <h1>edSHOP</h1>
       </div>
       <div className="header__search">
-
+        <input className="search__input" type="text" />
       </div>
       <div className="header__nav">
         <div className="nav__item">
