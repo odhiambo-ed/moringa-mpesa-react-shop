e3c87324255b5490f50a6d16e5e22a61cc8f1b75
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-02 23:59:45 +0300

    transfer header

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
 import './App.css';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import StoreIcon from '@mui/icons-material/Store';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <div className="header__logo">
-        <StoreIcon className='header__logoImage'/>
-        <h2 className='header__logoTitle'>edSHOP</h2>
-      </div>
-      <div className="header__search">
-        <input className="search__input" type="text" />
-        <SearchOutlinedIcon />
-      </div>
-      <div className="header__nav">
-        <div className="nav__item">
-          <span className="item__lineOne">
-            Hi Guest
-          </span>
-          <span className="item__lineTwo">
-            Sign In
-          </span>         
-        </div>
-        <div className="nav__item">
-          <span className="item__lineOne">
-            Your
-          </span>
-          <span className="item__lineTwo">
-            Shop
-          </span>
-        </div>
-        <div className="nav__item">
-          <span className="item__lineOne">
-            <AddShoppingCartIcon />
-          </span>
-          <span className="item__lineTwo">
-            1
-          </span>
-        </div>
-      </div>
+      <Header />
     </div>
   );
 }
