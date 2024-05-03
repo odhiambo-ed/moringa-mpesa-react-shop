df4efd0adc0ea56e5dc15b959fd5631c7c096f45
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-03 23:17:20 +0300

    add product info

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Product.css'
+import Watch from '../../images/p1.png'
 
 function Product() {
   return (
-    <div>Product</div>
+      <div className='product'>
+          <div className="product__info">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error impedit odio quisquam iste voluptates eum quas rerum quo ipsa veritatis.</p>
+              <p className="product__price">
+                  <small>Ksh</small>
+                  <strong>300.1</strong>
+              </p>
+                <div className="product__rating">
+                    <p>⭐</p>
+                    <p>⭐</p>
+                  <p>⭐</p>
+                </div>
+          </div>
+          <img src={Watch} alt="" className='product__image' />
+            <button>Add to Cart</button>
+    </div>
   )
 }
 
