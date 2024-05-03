a49e8acbc36c657f19e380c74089456ca6a1332e
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-03 22:48:52 +0300

    add hero image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Home.css'
-import Hero from '../../images/hero-ecom.jpg'
+import Hero from '../../images/hero-ecom-2.jpg'
 import Product from '../Product/Product'
 
 function Home() {
@@ -8,18 +8,18 @@ function Home() {
     <div className='home'>
       <div className="home__container">
         <img src={Hero} alt="" className='home__image' />
-      </div>
-      <div className="home__row">
-        <Product />
-        <Product />
-      </div>
-      <div className="home__row">
-        <Product />
-        <Product />
-        <Product />
-      </div>
-      <div className="home__row">
-        <Product />
+        <div className="home__row">
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+        </div>
       </div>
     </div>
   )
