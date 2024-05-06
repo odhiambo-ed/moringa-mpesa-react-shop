77418626a05346839f4d4d612bb9c01d9d1d4166
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-06 09:45:01 +0300

    passing props on product

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Home.css'
 import Hero from '../../images/hero-ecom-2.jpg'
 import Product from '../Product/Product'
+import Watch from '../../images/p1.png'
 
 function Home() {
   return (
@@ -9,16 +10,52 @@ function Home() {
       <div className="home__container">
         <img src={Hero} alt="" className='home__image' />
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+            id='123'
+            title='The Lean Startup'
+            price={29.99}
+            rating={5}
+            image={Watch}
+          />
+          <Product
+            id='123'
+            title='The Lean Startup'
+            price={29.99}
+            rating={5}
+            image={Watch}
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id='123'
+            title='The Lean Startup'
+            price={29.99}
+            rating={5}
+            image={Watch}
+          />
+          <Product
+            id='123'
+            title='The Lean Startup'
+            price={29.99}
+            rating={5}
+            image={Watch}
+          />
+          <Product
+            id='123'
+            title='The Lean Startup'
+            price={29.99}
+            rating={5}
+            image={Watch}
+          />
         </div>
         <div className="home__row">
-          <Product />
+          <Product
+            id='123'
+            title='The Lean Startup'
+            price={29.99}
+            rating={5}
+            image={Watch}
+          />
         </div>
       </div>
     </div>
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import './Product.css'
-import Watch from '../../images/p1.png'
-
-function Product() {
+import React from "react";
+import "./Product.css";
+function Product({ id, title, price, rating, image }) {
   return (
-      <div className='product'>
-          <div className="product__info">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error impedit odio quisquam iste voluptates eum quas rerum quo ipsa veritatis.</p>
-              <p className="product__price">
-                  <small>Ksh</small>
-                  <strong>300.1</strong>
-              </p>
-                <div className="product__rating">
-                    <p>⭐</p>
-                    <p>⭐</p>
-                  <p>⭐</p>
-                </div>
-          </div>
-          <img src={Watch} alt="" className='product__image' />
-            <button>Add to Cart</button>
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>Ksh</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+      <img src={image} alt="" className="product__image" />
+      <button>Add to Cart</button>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
