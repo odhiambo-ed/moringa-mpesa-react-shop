04071f2ac06318a065068d018f2bdb997377d300
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-06 11:04:58 +0300

    add routes to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path='/Cart'>
+            <h1>Checkout</h1>
+          </Route>
+          <Route path='/login'>
+            <h1>Login</h1>
+          </Route>
+          <Route path='/'>
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
       <Header />
       <Home />
     </div>
