9b3e3c90a86689586fab64ceb98a0f1321785421
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-07 08:48:35 +0300

    add cart total section

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -2,18 +2,22 @@ import React from 'react'
 import 'cart.css'
 import { Camera } from '../../images/p3.png'
 import CartItems from '../CartItems'
+import CartTotal from '../CartTotal'
 
 function index() {
   return (
     <div className="checkout">
       <div className="checkout__left">
-        <img src={Camera} alt="" />
+        <img src={Camera} alt="" className='checkout__ad' />
         <dir>
           <h2 className="checkout__title">
             Your shopping Basket
           </h2>
           <CartItems />
         </dir>
+      </div>
+      <div className="checkout__right">
+        <CartTotal />
       </div>
     </div>
   )
