df8ceedc9bcf004d001faed20ed8e87bed7176b1
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-07 09:16:37 +0300

    add cart route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import { Routes, Route } from 'react-router-dom';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/checkout" element={<h1>Checkout</h1>} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<h1>Login</h1>} />
         <Route path="/" element={<><Header /><Home /></>} />
       </Routes>
