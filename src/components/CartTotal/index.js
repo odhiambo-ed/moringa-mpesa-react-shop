75ff4e099215ac1d1cb6900989e8e1f1d831d648
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-07 09:09:49 +0300

    add subtotal value

--- src/components/CartTotal/index.js
+++ src/components/CartTotal/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import 'cart_total.css'
+import CurrencyFormat from 'react-currency-format'
 
 function index() {
   return (
-    <div>index</div>
+      <div className="subtotal">
+          <CurrencyFormat
+              renderText={(value) => (
+                  <>
+                      <p>
+                          Subtotal (0 items): <strong>0</strong>
+                      </p>
+                      <small className="subtotal__gift">
+                          <input type="checkbox" /> This order contains a gift
+                      </small>
+                  </>
+              )}
+              decimalScale={2}
+              value={0}
+              displayType={"text"}
+              thousandSeparator={true}
+              prefix={"Ksh"}
+          />
+          <button>Proceed to Checkout</button>
+    </div>
   )
 }
 
