a5c23745d647cf9998845753967b078b0ec1a2f1
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-07 08:39:58 +0300

    display products on cart page

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import 'cart.css'
+import { Camera } from '../../images/p3.png'
+import CartItems from '../CartItems'
 
 function index() {
   return (
-    <div>index</div>
+    <div className="checkout">
+      <div className="checkout__left">
+        <img src={Camera} alt="" />
+        <dir>
+          <h2 className="checkout__title">
+            Your shopping Basket
+          </h2>
+          <CartItems />
+        </dir>
+      </div>
+    </div>
   )
 }
 
