81ee5a02b20dd32bb58906c03e6b0796b0a65d21
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-07 08:29:30 +0300

    add cart items components

---
+++ src/components/CartItems/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import 'cartItems.css'
+
+function index(id, title, price, rating, image) {
+  return (
+      <div className="cart__items">
+          <div className="product__info">
+              <p>{title}</p>
+              <p className="product__price">
+                  <small>Ksh</small>
+                  <strong>{price}</strong>
+              </p>
+              <div className="product__rating">
+                  {Array(rating)
+                      .fill()
+                      .map((_, i) => (
+                          <p>⭐</p>
+                      ))}
+              </div>
+          </div>
+          <img src={image} alt="" className="product__image" />
+          <button>Add to Cart</button>
+      </div>
+  )
+}
+
+export default index
