1fd21c1f2fd36baa06af2402f745844b3553ff9b
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-05-09 11:51:17 +0300

    edit index js

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './cart.css'
-import Camera from '../../images/p3.png'
 import CartItems from '../CartItems'
 import CartTotal from '../CartTotal'
 
@@ -8,7 +7,7 @@ function index() {
   return (
     <div className="checkout">
       <div className="checkout__left">
-        <img src={Camera} alt="" className='checkout__ad' />
+        <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout__ad" />
         <dir>
           <h2 className="checkout__title">
             Your shopping Basket
